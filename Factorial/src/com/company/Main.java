1ec9aab2e6aad33a49510e1f6deea7385a716249
Nouraoqail <Nouraoqail.10@gmail.com>
2021-11-14 20:24:43 +0300

    HW done

---
+++ Factorial/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        if(num>0) {
+            int i = 1, fact = 1;
+            while (i <= num) {
+                fact = fact * i;
+                i++;
+            }
+            System.out.println("Factorial of the number: " + fact);
+        }else
+        System.out.println("the number is not positive");
+
+
+    }
+}
